d6c1efb516672438d4efe212687a5d3f7336e727
Fernando <Fercamposo98@gmail.com>
2020-08-16 19:40:39 +0000

    l04

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const cool = require("cool-ascii-faces");
+const express = require("express");
 
+var app = express();
 
+app.get("/cool",(req,res) => {
+	res.send("<html>"+cool()+"</html>");
+});
+		
+app.listen(80);
 
-
-
-console.log(cool());
+console.log("server ready");
