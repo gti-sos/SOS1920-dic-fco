885a8dc5ae4a44056b70d3c972284bf44e7796b7
Fernando <Fercamposo98@gmail.com>
2020-08-18 16:39:11 +0000

    heroku

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const express = require("express");
 
 var app = express();
 
+
+var port = process.env.PORT || 80;
+
+app.use("/",express.static("./public"))
+
 app.get("/cool",(req,res) => {
 	res.send("<html>"+cool()+"</html>");
 });
 		
-app.listen(80);
+app.listen(80, ()=>{
+	console.log("Server ready")
+});
 
-console.log("server ready");
+console.log("Starting server");
